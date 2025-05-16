58b22e1ed421218ef6188b62584c3a5d3820ae20
arshath2014 <arshathsheikabdullah@gmail.com>
2025-05-16 15:59:31 +0530

    JAVAscript

---
+++ script.js
@@ -0,0 +1,31 @@
+var a=10
+a="apple"
+console.log(a)
+let b=10
+ b='react'
+
+ function test(c,b,d){
+
+    var a=c+b
+    a=c+b+d
+    console.log(a)
+ }
+
+console.log(b)
+
+test(20,30,10)
+test(20,60)
+var arr=[10,20,'cdm laksd',50] 
+console.log(arr)
+arr [3]=70
+
+function check(){
+ 
+    
+    for(i=0;i<=5;i++){
+console.log(a)
+}
+    }
+
+check()
+        
